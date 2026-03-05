680f5f4ffe7ebf250f932153f82cfccc0e29554c
YaakovHai <kobi8471@gmail.com>
2026-03-05 18:48:38 +0200

    add fonction

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+from re import sub
+
+
 print("hello, word!")
+
+
+# print(2 + 3)
+# print(2 * 3)
+# print(2 / 3)
+# print(2 - 3)
+
+# USER_NAME = input("What is your name? ")
+# print("Hello, " + USER_NAME + "!")
+
+# def greet(name):
+#     print("Hello, " + name + "!")
+
+# greet ("liron")
+# greet ("dana")
+# greet ("shira")
+# greet ("yossi")
+# greet ("michael")
+# greet ("noa yaakov")
+
+def add(num1,num2):
+    print(num1 + num2)
+
+def sub(num1,num2):
+    print(num1 - num2)
+
+add(8,5)
+add(10, 15)
+add(100, 200)
+add(1.5, 2.5)
+sub(5,8)
